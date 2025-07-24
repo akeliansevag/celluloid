7efe238eda5114363f04d1501bf5036800a66448
akeliansevag <akelian.sevag@gmail.com>
2025-07-24 15:41:14 +0300

    .

--- components/pages/(home)/home-1/testimonial.tsx
+++ components/pages/(home)/home-1/testimonial.tsx
@@ -99,7 +99,7 @@ const Testimonial = () => {
                             {testimonials.map((testimonial, i) => (
                                 <SwiperSlide key={i}>
                                     <div className="testimonial-content">
-                                        <p dangerouslySetInnerHTML={{ __html: testimonial.content }} />
+                                        <p dangerouslySetInnerHTML={{ __html: testimonial.content || "" }} />
                                         {/* <div className="author">
                                             <h5 className="author-title">
                                                 {testimonial.name}
